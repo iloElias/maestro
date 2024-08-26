5bf3a0e965ed8f8f1f40ce760026ec1aad073b51
iloElias <murilo7456@gmail.com>
2024-08-26 14:40:07 -0300

    [pre] - sql query bulder

--- src/Abstract/Sql.php
+++ src/Abstract/Sql.php
@@ -6,12 +6,13 @@ abstract class Sql
 {
   public abstract function getSql(): string;
   public abstract function getParameters(): array;
-  public function bindParameters(): void
+  public function bindParameters(): string
   {
     $params = $this->getParameters();
     $stmt = $this->getSql();
     foreach ($params as $key => $value) {
       $stmt = str_replace($key, $value, $stmt);
     }
+    return $stmt;
   }
 }
