90a5163f8eca2a8e2405407ffece3faab342e04b
Murilo Elias <muriloelias@ipeweb.com.br>
2024-09-26 08:33:50 -0300

    [sync] - syncronize master with latest version

---
+++ src/Core/Maestro.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ilias\Maestro\Core;
+
+final class Maestro
+{
+  public const SQL_STRICT = 'STRICT';
+  public const SQL_PREDICT = 'PREDICT';
+  public const SQL_NO_PREDICT = 'NO_PREDICT';
+  public const DOC_PRIMARY = '@primary';
+  public const DOC_FOREIGN = '@foreign';
+  public const DOC_UNIQUE = '@unique';
+  public const DOC_NUABLE = '@nuable';
+  public const DOC_NOT_NUABLE = '@not_nuable';
+  public const DOC_COMMENT = '@comment';
+
+  public const MAETRO_DOC_CLAUSES = [
+    self::DOC_UNIQUE,
+    self::DOC_NUABLE,
+    self::DOC_NOT_NUABLE,
+    self::DOC_PRIMARY,
+    self::DOC_FOREIGN,
+  ];
+
+  public const PREFER_DEFAULT = 0;
+  public const PREFER_DOC = 1;
+}
